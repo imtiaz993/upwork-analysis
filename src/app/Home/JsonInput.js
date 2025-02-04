49a72cbd5691cf101b6e75f18f25bacde74c8b9c
Imtiaz Hussain <57477671+imtiaz993@users.noreply.github.com>
2025-02-04 21:09:01 +0500

    added meta

--- src/app/Home/JsonInput.js
+++ src/app/Home/JsonInput.js
@@ -1,21 +1,32 @@
 import React from "react";
+import { sampleData } from "./data";
 
 export default function JsonInput({ jsonInput, setJsonInput, onParseJson }) {
   return (
-    <div className="mb-4 flex">
-      <textarea
-        className="w-full text-sm p-2 border border-gray-300 rounded resize-none outline-none"
-        placeholder="Paste JSON here..."
-        rows={1}
-        value={jsonInput}
-        onChange={(e) => setJsonInput(e.target.value)}
-      />
-      <button
-        onClick={onParseJson}
-        className="ml-2 px-4 py-1 bg-blue-600 text-white rounded text-sm font-semibold"
+    <div className="mb-4">
+      <div className="flex mb-1">
+        <textarea
+          className="w-full text-sm p-2 border border-gray-300 rounded resize-none outline-none"
+          placeholder="Paste JSON here..."
+          rows={1}
+          value={jsonInput}
+          onChange={(e) => setJsonInput(e.target.value)}
+        />
+        <button
+          onClick={onParseJson}
+          className="ml-2 px-4 py-1 bg-blue-600 text-white rounded text-sm font-semibold"
+        >
+          Parse
+        </button>
+      </div>
+      <p
+        className="ml-1 text-sm cursor-pointer"
+        onClick={() => {
+          setJsonInput(JSON.stringify(sampleData));
+        }}
       >
-        Parse
-      </button>
+        Load Sample Data
+      </p>
     </div>
   );
 }
